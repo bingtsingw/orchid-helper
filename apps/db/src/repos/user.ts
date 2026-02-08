dc869e898f291c5f2542f20eebd7f968f54cf35b
hsj <tank2308635@163.com>
2026-02-08 12:50:36 +0800

    feat: fix special chart

--- apps/db/src/repos/user.ts
+++ apps/db/src/repos/user.ts
@@ -9,31 +9,8 @@ const selectForDefault = db.user.makeHelper((q) => {
 
 export const user = createRepo(db.user, {
   queryMethods: {
-    whereByProfileName: (q, keyword: string) => {
-      // undefined or null -> search for null profileName
-      if (keyword === undefined || keyword === null) {
-        return q.where({ id: '__NOT_EXIST__' });
-      }
-
-      const _keyword = String(keyword);
-
-      if (_keyword) {
-        return q.where(
-          q.sql<boolean>`(${q.column('profile')}->>'name')::text ILIKE $keyword`.values({
-            keyword: `%${_keyword.replace(/([\%_])/g, '\\$1')}%`,
-          }),
-        );
-      } else {
-        return q.where(
-          q.sql<boolean>`(${q.column('profile')}->>'name')::text = $keyword`.values({
-            keyword: _keyword,
-          }),
-        );
-      }
-    },
-
     whereSearchName: (q, keyword?: string) => {
-      // undefined or null -> search for null profileName
+      // undefined or null -> search for null id
       if (keyword === undefined || keyword === null) {
         return q.where({ id: '__NOT_EXIST__' });
       }
